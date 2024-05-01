8ba6fa96377e139bbb6e22de144d62f2df3afc8e
Zhengda-Huang <huang.zhen@northeastern.edu>
2024-05-01 17:10:59 -0400

    minor

--- app/page.tsx
+++ app/page.tsx
@@ -76,7 +76,7 @@ export default function Home() {
           <ul className='flex flex-wrap justify-center items-center text-xl lg:text-2xl overflow-hidden'>
             {
               skills.map((skill, index) => (
-                <li key={index} className='w-full lg:w-1/3 xl:w-1/4 px-4 mb-8'>
+                <li key={index} className='w-full md:w-1/2 lg:w-1/3 xl:w-1/4 px-4 mb-8'>
                   <Card icon={skill.icon} title={skill.title} description={skill.description}  proficiency={skill.proficiency}/>
                 </li>
               ))
